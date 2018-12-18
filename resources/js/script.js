c2aca0f3775a78f7289d8f962c2a23deacbf190a
td94 <quynhtrangdo94@gmail.com>
2018-12-18 01:35:13 -0500

    animation on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,34 +6,56 @@ $('.js--section-features').waypoint(function (direction) {
         $('nav').removeClass('sticky')
     }
 }, {
-    offset: '60px'
-})
+        offset: '60px'
+    })
 
 //header buttons
-$('.js--scroll-to-plans').click(function(){
+$('.js--scroll-to-plans').click(function () {
     $('html, body').animate({
         scrollTop: $('.js--section-plans').offset().top
     }, 1000)
 })
 
-$('.js--scroll-to-start').click(function(){
+$('.js--scroll-to-start').click(function () {
     $('html, body').animate({
         scrollTop: $('.js--section-features').offset().top
     }, 1000)
 })
 
 //smooth scrolling
-$(function() {
-    $('a[href*=#]').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 2000);
-          return false;
+$(function () {
+    $('a[href*=#]').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 2000);
+                return false;
+            }
         }
-      }
     });
-  });
+});
+
+//animation on scroll
+$('.js--wp-1').waypoint(function(direction){
+    $('.js--wp-1').addClass('animated fadeIn')
+},{
+    offset: '50%'
+})
+$('.js--wp-2').waypoint(function(direction){
+    $('.js--wp-2').addClass('animated fadeInUp')
+},{
+    offset: '50%'
+})
+$('.js--wp-3').waypoint(function(direction){
+    $('.js--wp-3').addClass('animated fadeIn')
+},{
+    offset: '50%'
+})
+$('.js--wp-4').waypoint(function(direction){
+    $('.js--wp-4').addClass('animated pulse')
+},{
+    offset: '50%'
+})
