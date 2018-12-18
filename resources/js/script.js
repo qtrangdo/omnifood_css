78e1f0f794a5498a600449ca6948df3a6829347c
td94 <quynhtrangdo94@gmail.com>
2018-12-18 00:36:27 -0500

    first commit, web designed

---
+++ resources/js/script.js
@@ -0,0 +1,10 @@
+$('.js--section-features').waypoint(function (direction) {
+    if (direction == "down") {
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeClass('sticky')
+    }
+}, {
+    offset: '60px'
+})
+
